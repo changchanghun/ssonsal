0f24f4ab77587fcfb5fd8a51aaf75ed732d6593f
changchanghun <ch4856lee@naver.com>
2025-09-27 22:04:41 +0900

    시큐리티 업데이트

--- src/main/java/chworld/ssonsal/config/SecurityConfig.java
+++ src/main/java/chworld/ssonsal/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/","/login","/register","/save","/check-email","/trip-end","/log").permitAll()
+                        .requestMatchers("/healthCheck").permitAll()
                         .requestMatchers("/js/**", "/css/**", "/images/**").permitAll()
                         .requestMatchers("/custom-logout").permitAll()
                         .requestMatchers("/api/email/**").permitAll()
